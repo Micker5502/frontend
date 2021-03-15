4730046e5e92c675c0d8ed135d22adeade33cbc1
micker5502 <zotto5502@hotmail.com>
2021-03-15 23:31:38 +0800

    -push

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,13 @@ module.exports = {
     "eslint:recommended",
     "@vue/typescript/recommended",
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
+    ecmaFeatures: {
+      legacyDecorators: true
+    },
     ecmaVersion: 2020
-  },
+  }, 
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
